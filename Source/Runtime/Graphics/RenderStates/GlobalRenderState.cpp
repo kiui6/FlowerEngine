37759f63f823d5c0f1620afa802e5d0bc9367516
きゐ <40644016+SasisaDev@users.noreply.github.com>
2026-05-31 19:13:32 +0300

    New default pixel perfect resolution

--- Source/Runtime/Graphics/RenderStates/GlobalRenderState.cpp
+++ Source/Runtime/Graphics/RenderStates/GlobalRenderState.cpp
@@ -7,6 +7,6 @@ GlobalRenderState::GlobalRenderState(GPUContext &gpu)
 {
     Config* config = GetService<Config>();
 
-    canvasWidth = config->GetNamespace("Game").GetInt("Render.PixelPerfect.CanvasWidth", 320);
-    canvasHeight = config->GetNamespace("Game").GetInt("Render.PixelPerfect.CanvasHeight", 180);
+    canvasWidth = config->GetNamespace("Game").GetInt("Render.DefaultCanvasWidth", 640);
+    canvasHeight = config->GetNamespace("Game").GetInt("Render.DefaultCanvasHeight", 360);
 }
