71fb8bb4459288a62c3739cb9515587276a5a9e5
きゐ <40644016+SasisaDev@users.noreply.github.com>
2026-06-22 14:50:33 +0300

    Updated Arena implementation

--- Source/Runtime/Utility/Memory/Arena.h
+++ Source/Runtime/Utility/Memory/Arena.h
@@ -5,26 +5,44 @@
 #include <cassert>
 
 
-// TODO Arena Implementation
+// TODO
+/* 
+ * An arena that has no fall back
+ * Once arena is exhausted subsequent allocations will fail
+ * Once failed, assertion is thrown and application is terminated 
+ *  
+ * BlockSize should be chosen to fit 100% of cases.
+ * This structure should be used if compile time guarantees are present
+ */
 template <size_t BlockSize>
-class Arena
+class StaticArena
 {
     std::unique_ptr<std::byte[]> m_memory;
-    size_t m_lastBlock = 0;
     size_t m_currentOffset = 0;
-
 public:
-    Arena() {
+    StaticArena() {
         m_memory = std::make_unique<std::byte[]>(BlockSize);
-        m_lastBlock = 0;
         m_currentOffset = 0;
     }
 
-    Arena(const Arena&) = delete;
-    Arena(Arena&&) = delete;
+    StaticArena(const StaticArena&) = delete;
+    StaticArena(StaticArena&& right) {
+        m_memory = std::move(right.m_memory);
+        m_currentOffset = std::exchange(right.m_currentOffset, 0);
+    }
 
     void* Allocate(size_t size, size_t align = alignof(std::max_align_t)) {
-        return nullptr;
+        uintptr_t addr = reinterpret_cast<uintptr_t>(m_memory.get()) + m_currentOffset;
+        size_t pad = (align - (addr % align)) % align;
+
+        if((pad + size) > (BlockSize - m_currentOffset)) [[unlikely]] {
+            assert(!"Allocation too large");
+            return nullptr;
+        }
+
+        m_currentOffset += pad + size;
+
+        return reinterpret_cast<void*>(addr + pad);
     }
 
     template<typename T>
@@ -39,35 +57,46 @@ public:
     }
 };
 
+/* 
+ * An arena that falls back to creating more contigious memory blocks
+ * Once latest block is exhausted it allocates a new block. 
+ * 
+ * After new allocations, cache efficiency drops near the seams
+ * BlockSize should be chosen to fit 80-90% of cases, with occasional fall backs to new blocks
+ */
 template <size_t BlockSize>
-class DynamicArena
+class GrowingArena
 {
     std::vector<std::unique_ptr<std::byte[]>> m_memBlocks;
     size_t m_lastBlock = 0;
     size_t m_currentOffset = 0;
 
 public:
-    DynamicArena() {
+    GrowingArena() {
         CreateNewBlock();
         m_lastBlock = 0;
         m_currentOffset = 0;
     }
 
-    DynamicArena(const DynamicArena&) = delete;
-    DynamicArena(DynamicArena&&) = delete;
+    GrowingArena(const GrowingArena&) = delete;
+    GrowingArena(GrowingArena&& right) {
+        m_memBlocks = std::move(right.m_memBlocks);
+        m_lastBlock = std::exchange(right.m_lastBlock, 0);
+        m_currentOffset = std::exchange(right.m_currentOffset, 0);
+    }
 
     void* Allocate(size_t size, size_t align = alignof(std::max_align_t)) {
         uintptr_t addr = reinterpret_cast<uintptr_t>(m_memBlocks[m_lastBlock].get()) + m_currentOffset;
         size_t pad = (align - (addr % align)) % align;
 
         // Return nullptr if requested size is more than block size
-        if(pad + size > BlockSize) {
+        if(pad + size > BlockSize) [[unlikely]] {
             assert(!"Allocation too large");
             return nullptr;
         }
 
         // Advance or create a new block before proceeding if requested memory doesn't fit current block
-        if(m_currentOffset + pad + size > BlockSize) {
+        if(m_currentOffset + pad + size > BlockSize) [[unlikely]] {
             // If last block in use is the last block available, create new block
             if(m_lastBlock == m_memBlocks.size() - 1) {
                 CreateNewBlock();
