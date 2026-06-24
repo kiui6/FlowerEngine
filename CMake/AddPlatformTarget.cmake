613ad9a053c6fbd476ed4868f858c6051a6cf9c3
きゐ <40644016+SasisaDev@users.noreply.github.com>
2026-06-24 18:40:58 +0300

    cmake MacOS bundling

--- CMake/AddPlatformTarget.cmake
+++ CMake/AddPlatformTarget.cmake
@@ -9,6 +9,7 @@ function(add_platform_target TARGET_NAME)
     endif()
 
     if(APPLE)
+        target_link_libraries(${TARGET_NAME} PRIVATE MoltenVK)
         set_target_properties(${TARGET_NAME} PROPERTIES MACOSX_BUNDLE TRUE)
         set_target_properties(${TARGET_NAME} PROPERTIES MACOSX_BUNDLE_INFO_PLIST 
             ${PROJECT_SOURCE_DIR}/Resources/Platforms/Mac/Info.plist.in)
@@ -19,7 +20,26 @@ function(add_platform_target TARGET_NAME)
             MACOSX_PACKAGE_LOCATION "Resources/vulkan/icd.d"
         )
 
+        add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy
+                "${MOLTENVK_DYLIB}"
+                "$<TARGET_BUNDLE_DIR:${TARGET_NAME}>/Contents/Frameworks/libMoltenVK.dylib"
+            COMMENT "Copying MoltenVK dylib to app bundle"
+        )
+
         target_compile_definitions(${TARGET_NAME} PRIVATE PLATFORM_MACOS)
+
+        add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E echo "#!/bin/bash" > "$<TARGET_BUNDLE_DIR:${TARGET_NAME}>/Contents/MacOS/FlowerWrapper"
+            # Disable Metal Heap to extend support for older hardware
+            COMMAND ${CMAKE_COMMAND} -E echo "export MVK_CONFIG_USE_MTLHEAP=0" >> "$<TARGET_BUNDLE_DIR:${TARGET_NAME}>/Contents/MacOS/FlowerWrapper"
+            # Specify icd file for loader
+            COMMAND ${CMAKE_COMMAND} -E echo "export VK_ICD_FILENAMES=\"$(dirname \"\$0\")/../Resources/vulkan/icd.d/MoltenVK_icd.json\"" >> "$<TARGET_BUNDLE_DIR:${TARGET_NAME}>/Contents/MacOS/FlowerWrapper"
+            # Launch real executable
+            COMMAND ${CMAKE_COMMAND} -E echo "exec \"$(dirname \"$0\")/${TARGET_NAME}\" \"\$@\"" >> "$<TARGET_BUNDLE_DIR:${TARGET_NAME}>/Contents/MacOS/FlowerWrapper"
+            COMMAND chmod +x "$<TARGET_BUNDLE_DIR:${TARGET_NAME}>/Contents/MacOS/FlowerWrapper"
+            COMMENT "Creating launch wrapper for macOS bundle"
+        )
     endif()
 
     if(UNIX AND NOT APPLE) 
